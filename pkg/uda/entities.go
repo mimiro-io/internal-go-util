ed6ba3a9996d1cf88d2ea6f27ac5ba14f9ef3a96
Bjørn Cintra <bjorn.tore.cintra@inmeta.no>
2022-06-21 14:03:33 +0200

    Adding some utility functions to entities

--- pkg/uda/entities.go
+++ pkg/uda/entities.go
@@ -1,5 +1,10 @@
 package uda
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Entity struct {
 	ID         string         `json:"id"`
 	IsDeleted  bool           `json:"deleted"`
@@ -20,4 +25,35 @@ type Context struct {
 	Namespaces      map[string]string `json:"namespaces"`
 	NamespaceLookup map[string]string `json:"omit"`
 	RDFs            map[string]string
+}
+
+// StripPrefixes will strip the namespace prefix from any property, making it
+// easier to work with
+func (e *Entity) StripPrefixes() map[string]any {
+	var properties = make(map[string]any)
+	// dechunk the fields
+	for k, v := range e.Properties {
+		properties[dechunk(k)] = v
+	}
+	return properties
+}
+
+// dechunk will remove the first chunk of the string
+func dechunk(s string) string {
+	v := s
+	if strings.Contains(v, ":") {
+		return strings.Split(v, ":")[1]
+	}
+	return s
+}
+
+// ToURI will expand a ref into a full uri, replacing the curie with the full uri
+func ToURI(context *Context, ref string) string {
+	ns, val, _ := strings.Cut(ref, ":")
+	l := context.Namespaces[ns]
+	if l == "" { // probably just a regular ":" in the value, leave it alone
+		return ref
+	}
+
+	return fmt.Sprintf("%s%s", l, val)
 }
